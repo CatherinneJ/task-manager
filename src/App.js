166b97185dedb02370885fd1469733ab183e95ba
Katarina Janikova <janikova2@gmail.com>
2025-07-23 14:56:44 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './app.css';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import Details from './pages/Details';
+import Profile from './pages/Profile';
+import Login from './pages/Login';
+import ProtectedRoute from './components/ProtectedRoute';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar /> 
+    
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>   
+          } />
+        </Routes>
     </div>
   );
+
+ 
 }
 
 export default App;
