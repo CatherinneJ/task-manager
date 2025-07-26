6c57e4246500b12a90a3b2f6349fb5a024c9e137
Katarina Janikova <janikova2@gmail.com>
2025-07-26 01:21:20 -0700

    Add quotes, text Home

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './app.css';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
@@ -25,6 +25,7 @@ function App() {
           } />
         </Routes>
     </div>
+
   );
 
  
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import './home.css';
 
 const Home = () => {
-  return <h1>Home Page</h1>;
+  return (
+  <div className="home-cont">
+    <p className="home-text">
+        "Every new day brings another start,a new beginning, 
+        a new world full of opportunities and possibilities". 
+      </p>
+      <p className="home-text">
+        (anonymous)
+        </p>
+  </div>
+);
 };
 
 export default Home;
